b090f61ceaa82e430406e44afd74fe0da6ffd87c
Dan Shoji <danshoji@MacBook-Air-de-Dan.local>
2026-09-10 16:22:26 -0300

    US01

--- Aula 2/cinema.py
+++ Aula 2/cinema.py
@@ -8,12 +8,13 @@ tipo_sala = {}
 
 class Filme:
 
-    def __init__(self, nome=None, data_estreia=None, data_saida=None, duracao=None):
-#        self.codigo
+    def __init__(self, codigo = None, nome=None, data_estreia=None, data_saida=None, duracao=None):
+        self.codigo = codigo
         self.nome = nome
         self.data_estreia = data_estreia
         self.data_saida = data_saida
-        self.duracao = duracao        
+        self.duracao = duracao
+        
 
 class Sala:
 
@@ -30,6 +31,12 @@ class Sessao:
         self.data = data
         self.hora_inicio = hora_inicio
 #        self.assentos
+
             
-    
+def cadastrar_filme(nome, data_estreia, data_saida, duracao):
+    num = len(filmes)
+    filme = Filme(num+1, nome, data_estreia, data_saida, duracao)
+    if (filme != None):
+        return filme
+    return None
 #metodos
--- Aula 2/tests.py
+++ Aula 2/tests.py
@@ -1,4 +1,5 @@
 import unittest
+from cinema import Filme, cadastrar_filme, filmes
 
 class TestModelo(unittest.TestCase):
 
@@ -7,8 +8,17 @@ class TestModelo(unittest.TestCase):
     def test_hello(self):
         self.assertEqual('hello','hello')
 
-    def test_goodbye(self):
-        self.assertTrue('goodbye'=='hello')
+    def test_cadastrar_filme(self):
+        codigo_esperado = len(filmes) + 1
+
+        filme = cadastrar_filme("Teste", "2002/09/13", "2005/10/21", 60)
+
+        self.assertIsInstance(filme, Filme)
+        self.assertEqual(filme.codigo, codigo_esperado)
+        self.assertEqual(filme.nome, "Teste")
+        self.assertEqual(filme.data_estreia, "2002/09/13")
+        self.assertEqual(filme.data_saida, "2005/10/21")
+        self.assertEqual(filme.duracao, 60)
 
 #    def tearDown(self):
 
